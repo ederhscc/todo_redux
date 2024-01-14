a1e0a970cc27d973840c5bfb077d39ddcf8129bc
Eder Henrique <ederhscc@gmail.com>
2024-01-14 12:34:33 -0300

    updated Addtodo component for to add task

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
+import {useDispatch} from "react-redux"
+import {addTodo} from "../slices/todoSlice"
 
 const AddTodo = () => {
-  const [task, setTask] = useState("");
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(input.trim() === "") return
+
+    dispatch(addTodo(input))
+    setInput("")
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Adicionar uma tarefa..."
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
       <button type="submit"> Enviar</button>
     </form>
