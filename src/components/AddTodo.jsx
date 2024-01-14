a5f058edcee5cd523465fe3d8e82ef14a566117b
Eder Henrique <ederhscc@gmail.com>
2024-01-14 11:18:50 -0300

    added the structures of the components

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useState } from "react";
 
 const AddTodo = () => {
+  const [task, setTask] = useState("");
   return (
-    <div>AddTodo</div>
-  )
-}
+    <form>
+      <input
+        type="text"
+        placeholder="Adicionar uma tarefa..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type="submit"> Enviar</button>
+    </form>
+  );
+};
 
-export default AddTodo
+export default AddTodo;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
-  )
-}
+    <div>
+      <button>Todas</button>
+      <button>Completas</button>
+      <button>Incompletas</button>
+      <ul>
+        <li>
+          <span>Algum texto de tarefa</span>
+          <button>Remover</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
